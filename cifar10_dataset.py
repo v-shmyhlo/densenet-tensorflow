0d9a79c7ddb3f5fe4fdf3c999344f6170c77ebfd
Vlad Shmyhlo <aion.stu@gmail.com>
2018-01-26 11:13:18 +0200

    Refactor

--- cifar10_dataset.py
+++ cifar10_dataset.py
@@ -44,3 +44,36 @@ def make_dataset(path):
   test_ds = test_ds.map(preprocessing)
 
   return train_ds, test_ds
+
+
+def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--dataset-path', type=str, required=True)
+  args = parser.parse_args()
+
+  train_ds, test_ds = make_dataset(args.dataset_path)
+  iter = tf.data.Iterator.from_structure(
+      (tf.float32, tf.int64),
+      ((32, 32, 3), ()),
+  )
+  train_init = iter.make_initializer(train_ds),
+  test_init = iter.make_initializer(test_ds),
+  x, y = iter.get_next()
+
+  with tf.Session() as sess:
+    sess.run(train_init)
+    img = sess.run(x)
+    plt.imshow(img)
+    plt.show()
+
+    sess.run(test_init)
+    img = sess.run(x)
+    plt.imshow(img)
+    plt.show()
+
+
+if __name__ == '__main__':
+  import argparse
+  import matplotlib.pyplot as plt
+
+  main()
